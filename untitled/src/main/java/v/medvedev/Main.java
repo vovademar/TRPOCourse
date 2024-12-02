813969e913662c17e4d99ec61b3a7a5bc27866d8
Медведев Вальдемар <valdmedv@gmail.com>
2024-12-02 10:42:50 +0300

    ci/cd

---
+++ untitled/src/main/java/v/medvedev/Main.java
@@ -0,0 +1,7 @@
+package untitled.src.main.java.v.medvedev;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ untitled/src/test/java/ExampleTest.java
@@ -0,0 +1,15 @@
+package untitled.src.test.java;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testSum() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
