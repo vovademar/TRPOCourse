4189f4ed8b92858bcb7ce8bf1a7a2beeaee33df3
Медведев Вальдемар <valdmedv@gmail.com>
2024-12-02 11:52:46 +0300

    test fix

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,9 +1,8 @@
 package src.test.java;
 
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExampleTest {
 
