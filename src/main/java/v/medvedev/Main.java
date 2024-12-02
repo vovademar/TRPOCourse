3ac67e8c1c869b62bd8796ded7161339a38eedf3
Медведев Вальдемар <valdmedv@gmail.com>
2024-12-02 13:17:37 +0300

    new test

--- src/main/java/v/medvedev/Main.java
+++ src/main/java/v/medvedev/Main.java
@@ -4,4 +4,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    /**
+     *
+     * @param a - first param
+     * @param b - second param
+     * @param c - third param
+     * @return sum of a, b, c
+     */
+    public int calculate(int a, int b, int c) {
+        return a + b + c;
+    }
 }
--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -2,6 +2,8 @@ package src.test.java;
 
 
 import org.junit.jupiter.api.Test;
+import src.main.java.v.medvedev.Main;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExampleTest {
@@ -10,5 +12,11 @@ public class ExampleTest {
     public void testSum() {
         int result = 2 + 3;
         assertEquals(5, result);
+    }
+    @Test
+    public void testMethod() {
+        Main main = new Main();
+        int sum = main.calculate(1,2,3);
+        assertEquals(6, sum);
     }
 }
