b31411036e47c9ba3af46918a1b8544dd9b743d6
vovademar <vovademar@users.noreply.github.com>
2024-12-15 19:51:58 +0000

    deploy: 8add1cc470ae8c628750d223bff1a163df93faf8

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"src.main.java.v.medvedev"}];updateSearchResults();
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"src.main.java.v.medvedev","l":"Main"}];updateSearchResults();
